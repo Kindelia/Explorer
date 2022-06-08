c29292541da2657331eefab427e45bb33f7ac79d
Rafael Santos <rafaelnsantos1@gmail.com>
2022-06-08 20:53:49 -0300

    add title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Menu from '../components/Menu'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="mx-auto max-w-full lg:max-w-4xl py-5 px-5">
+      <Head>
+        <title>Kind Explorer</title>
+        <link
+          rel="shortcut icon"
+          href="https://kindelia.org/static/images/kindelia_icon.svg"
+        />
+      </Head>
       <Menu />
       <Component {...pageProps} />
     </div>
