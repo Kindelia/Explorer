2735543231e75cce2f14e8e40ce10b082e538732
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-06-24 15:49:01 -0300

    SelectNode colors

--- kexplorer/components/Navbar/SelectNode.tsx
+++ kexplorer/components/Navbar/SelectNode.tsx
@@ -30,17 +30,17 @@ export const SelectNode: FC = () => {
   return (
     <Menu
       as="div"
-      className="border-2 border-transparent px-1 mx-2 my-2 rounded-md font-medium relative bg-gray-100 border-x border-y border-gray-600"
+      className="border-2 px-1 mx-2 my-2 rounded-md font-medium relative border-x border-y border-font-light dark:border-font-dark"
     >
       <Menu.Button className="w-28 text-sm">{selectedNode.name}</Menu.Button>
       <DropdownTransition>
-        <Menu.Items className="z-10 flex flex-col absolute mt-2 w-28 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="z-10 flex flex-col absolute mt-2 w-28 rounded-md shadow-lg py-1 ring-1 ring-font-light dark:ring-font-dark focus:outline-none">
           {nodes.map((node) => (
             <Menu.Item key={node.name} disabled={selectedNode === node}>
               {({ disabled }) => (
                 <button
                   className={classNames(
-                    disabled ? 'text-gray-500' : 'hover:bg-gray-100',
+                    disabled ? 'font-bold border-8 hover:bg-searchbar-light dark:hover:bg-searchbar-dark border-searchbar-light dark:border-searchbar-dark' : 'hover:bg-searchbar-light dark:hover:bg-searchbar-dark',
                     'text-sm py-2'
                   )}
                   disabled={disabled}
