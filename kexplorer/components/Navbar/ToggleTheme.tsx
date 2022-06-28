ab816cd37e64bdcee1d7111df833f35b3fdd7428
Rafael Santos <rafaelnsantos1@gmail.com>
2022-06-28 16:22:22 -0300

    fix theme button

--- kexplorer/components/Navbar/ToggleTheme.tsx
+++ kexplorer/components/Navbar/ToggleTheme.tsx
@@ -13,21 +13,21 @@ export default function ToggleTheme() {
   }
 
   return (
-    <div className="self-center flex pr-2">
-      <button id="toggleTheme" onClick={toggleTheme}>
-        <label htmlFor="toggleTheme" className="cursor-pointer">
-          {mounted && (
-            <div className="w-9 h-5 flex items-center bg-gray-300 rounded-full relative">
-              <div
-                className={classNames(
-                  'w-4 h-4 bg-white rounded-full shadow absolute transition-all',
-                  theme === 'light' ? 'right-5' : 'right-0'
-                )}
-              />
-            </div>
-          )}
-        </label>
-      </button>
-    </div>
+    <button
+      onClick={toggleTheme}
+      id="toggleTheme"
+      className="self-center flex pr-2"
+    >
+      {mounted && (
+        <div className="w-9 h-5 flex items-center bg-gray-300 rounded-full relative">
+          <div
+            className={classNames(
+              'w-4 h-4 bg-white rounded-full shadow absolute transition-all',
+              theme === 'light' ? 'right-5' : 'right-0'
+            )}
+          />
+        </div>
+      )}
+    </button>
   )
 }
